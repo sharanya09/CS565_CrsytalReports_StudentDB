fef9ab815b537df5fe56eca212ac362e1db7da17
unknown <SharanyaC@W540-05797.concur.concurtech.org>
2015-12-05 14:06:02 -0800

    CrystalReports

---
+++ CS565_CrsytalReports_StudentDB/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace PRG25_STUD_CRYSTAL_DEMO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.txt_sub3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_sub2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_sub1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_rollno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.btn_all = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btn_submit);
+            this.panel1.Controls.Add(this.txt_sub3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txt_sub2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txt_sub1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txt_name);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txt_rollno);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 388);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btn_submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_submit.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.btn_submit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_submit.Location = new System.Drawing.Point(123, 306);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(88, 32);
+            this.btn_submit.TabIndex = 22;
+            this.btn_submit.Text = "SUBMIT";
+            this.btn_submit.UseVisualStyleBackColor = false;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // txt_sub3
+            // 
+            this.txt_sub3.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.txt_sub3.Location = new System.Drawing.Point(123, 239);
+            this.txt_sub3.Name = "txt_sub3";
+            this.txt_sub3.Size = new System.Drawing.Size(220, 29);
+            this.txt_sub3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(6, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "SUB3:-";
+            // 
+            // txt_sub2
+            // 
+            this.txt_sub2.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.txt_sub2.Location = new System.Drawing.Point(123, 184);
+            this.txt_sub2.Name = "txt_sub2";
+            this.txt_sub2.Size = new System.Drawing.Size(220, 29);
+            this.txt_sub2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(6, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "SUB2:-";
+            // 
+            // txt_sub1
+            // 
+            this.txt_sub1.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.txt_sub1.Location = new System.Drawing.Point(123, 129);
+            this.txt_sub1.Name = "txt_sub1";
+            this.txt_sub1.Size = new System.Drawing.Size(220, 29);
+            this.txt_sub1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(6, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SUB1:-";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.txt_name.Location = new System.Drawing.Point(123, 74);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(220, 29);
+            this.txt_name.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(6, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NAME:-";
+            // 
+            // txt_rollno
+            // 
+            this.txt_rollno.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.txt_rollno.Location = new System.Drawing.Point(123, 19);
+            this.txt_rollno.Name = "txt_rollno";
+            this.txt_rollno.Size = new System.Drawing.Size(220, 29);
+            this.txt_rollno.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ROLL NO:-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(65, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(238, 24);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "STUDENT INFORMATION";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_all);
+            this.groupBox1.Controls.Add(this.btn_search);
+            this.groupBox1.Controls.Add(this.txt_search);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(454, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(625, 56);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SEARCH";
+            // 
+            // btn_search
+            // 
+            this.btn_search.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btn_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_search.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.btn_search.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_search.Location = new System.Drawing.Point(363, 14);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(88, 32);
+            this.btn_search.TabIndex = 27;
+            this.btn_search.Text = "SEARCH";
+            this.btn_search.UseVisualStyleBackColor = false;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // txt_search
+            // 
+            this.txt_search.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.txt_search.Location = new System.Drawing.Point(123, 17);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(220, 29);
+            this.txt_search.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Window;
+            this.label7.Location = new System.Drawing.Point(6, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 24);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "ROLL NO:-";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.crystalReportViewer1);
+            this.groupBox2.Location = new System.Drawing.Point(371, 71);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(817, 450);
+            this.groupBox2.TabIndex = 26;
+            this.groupBox2.TabStop = false;
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(3, 16);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.SelectionFormula = "";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(811, 431);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ViewTimeSelectionFormula = "";
+            // 
+            // btn_all
+            // 
+            this.btn_all.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btn_all.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_all.Font = new System.Drawing.Font("Consolas", 13.75F, System.Drawing.FontStyle.Bold);
+            this.btn_all.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_all.Location = new System.Drawing.Point(484, 14);
+            this.btn_all.Name = "btn_all";
+            this.btn_all.Size = new System.Drawing.Size(135, 32);
+            this.btn_all.TabIndex = 28;
+            this.btn_all.Text = "ALL RECORD";
+            this.btn_all.UseVisualStyleBackColor = false;
+            this.btn_all.Click += new System.EventHandler(this.btn_all_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1200, 544);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_rollno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.TextBox txt_sub3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_sub2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_sub1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Button btn_all;
+    }
+}
+
---
+++ CS565_CrsytalReports_StudentDB/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+namespace PRG25_STUD_CRYSTAL_DEMO
+{
+    public partial class Form1 : Form
+    {
+		SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\dbStudent.mdf;Integrated Security=True");
+        SqlCommand com;
+        SqlDataAdapter adp = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            com = new SqlCommand("insert into tbl_student values ("+txt_rollno.Text+",'"+int.Parse(txt_name.Text)+"','"+int.Parse(txt_sub1.Text)+"','"+int.Parse(txt_sub2.Text)+"','"+int.Parse(txt_sub3.Text)+"')",con);
+            int x = com.ExecuteNonQuery();
+            con.Close();
+            if (x > 0)
+            {
+                ds.Tables[0].Rows.Clear();
+                CrystalAddData();
+                MessageBox.Show("Record Inserted");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            CrystalAddData();
+
+        }
+
+        public void CrystalAddData()
+        {
+            adp = new SqlDataAdapter("select * from tbl_student", con);
+            adp.Fill(ds);
+            ReportDocument rd = new ReportDocument();
+
+			rd.Load(@"C:\Users\SharanyaC\Desktop\PRG25_STUD_CRYSTAL_DEMO\PRG25_STUD_CRYSTAL_DEMO\CrystalReport1.rpt");
+            //rd.Load(Application.StartupPath+"CrystalReport1.rpt");
+            rd.SetDataSource(ds.Tables[0]);
+            crystalReportViewer1.ReportSource = rd;   
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            if (txt_search .Text == "")
+            {
+                MessageBox.Show("Enter Roll No");
+            }
+            else
+            {
+                ds.Tables[0].Rows.Clear();
+                adp = new SqlDataAdapter("select * from tbl_student where sno=" + txt_search .Text + "", con);
+                adp.Fill(ds);
+                ReportDocument rd = new ReportDocument();
+            //    rd.Load(Application.StartupPath+"CrystalReport1.rpt");
+				rd.Load(@"C:\Users\SharanyaC\Desktop\PRG25_STUD_CRYSTAL_DEMO\PRG25_STUD_CRYSTAL_DEMO\CrystalReport1.rpt");
+                rd.SetDataSource(ds.Tables[0]);
+                crystalReportViewer1.ReportSource = rd;
+            }
+        }
+
+        private void btn_all_Click(object sender, EventArgs e)
+        {
+            ds.Tables[0].Rows.Clear();
+            CrystalAddData();
+        }
+    }
+}
